FrostbyteGR/check_dht22

commit 7a2e9ca5241405c1867d1a6cd99361db088b05d5

Ensure a fresh timeslice before querying the sensor
This should make the check a little bit more reliable, but not 100%

dht22.c
@@ -26,6 +26,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <sys/time.h>
 
 // wiringPi library
@@ -39,7 +40,7 @@
 #define	FALSE (1==2)
 
 // Sensor definitions
-#define QUERYRETRIES 3
+#define QUERYRETRIES 2
 
 // Function to enforce delay until the GPIO transitions from LOW to HIGH state
 static int sensorLowHighWait(int GPIO) {
@@ -116,6 +117,11 @@ static int querySensor(int GPIO, unsigned int results[4]) {
 	struct timeval now, then, took;
 	unsigned int queryChecksum=0, retrievedBytes[5];
 	int byte;
+
+	// Ensure the query is guaranteed a fresh timeslice
+	// threadSleepTime = expectedExecutionTime * kernelInterruptFrequency
+	// x = 16ms * 250Hz
+	nanosleep((const struct timespec[]){{4,0}}, NULL);
 
 	// Take a timestamp before the operation begins
 	gettimeofday(&then, NULL);
@@ -221,9 +227,6 @@ struct sensorOutput parseSensorOutput(int GPIO) {
 				return result;
 			}
 		}
-
-		// Wait 2 seconds before retrying
-		delay(2000);
 	}
 
 	// Return the processed output

commit 40618dbc6d584396d1c48988af87499785830ed1

Fix regression caused by 7a2e9ca
Invalid results should no longer be able to find their way to the output

dht22.c
@@ -187,10 +187,6 @@ struct sensorOutput parseSensorOutput(int GPIO) {
 	struct sensorOutput result;
 	unsigned int sensorData[4];
 
-	// Set the sensor output defaults
-	result.temperature=SENSOR_NA;
-	result.humidity=SENSOR_NA;
-
 	// If wiringPi fails to initialize
 	if (wiringPiSetup()==-1) {
 		// Throw an error and exit
@@ -228,6 +224,12 @@ struct sensorOutput parseSensorOutput(int GPIO) {
 			}
 		}
 	}
+
+	// If this part is reached, no measurement was valid
+
+	// Set the output values to N/A
+	result.temperature=SENSOR_NA;
+	result.humidity=SENSOR_NA;
 
 	// Return the processed output
 	return result;